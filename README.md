e5e5303b14e84c41d96bc22035b818fc526a0cc8
Omar Hawwash <81506354+OmarMHawash@users.noreply.github.com>
2024-10-06 23:01:51 +0300

    initial structure

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CV Project
- 
+
+### Prerequisites:
+
+- recommended Python v`3.11.9` or newer.
+
+### Local Installation Steps:
+
+1. Clone the project: `git clone <repo_url>`
+2. Setup python app:
+
+```
+python -m venv .venv
+.\.venv\Scripts\activate
+pip install -r .\requirements.txt
+```
---
+++ run.py
@@ -0,0 +1,13 @@
+#TODO: 
+## User GUI for interaction
+#*   - Select multiple Images
+#*   - Select specific Features
+#*   - Adjust parameters
+#*   - Visualy comapre results
+
+## Feature-1 : Image Stitching
+#*   - Canny Edge Detection
+#*   - Difference of Gaussian
+#*   - Adjustable morphological operation
+#*   - Adjustable threshold
+#*   - AI edge detector to recognize humans
